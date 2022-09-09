104c992f06fa6c8305fa3fb87510a313f2bdc355
Samit Koyom <samitkoyom@gmail.com>
2022-09-10 00:52:59 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-green-700 dark:bg-black h-screen">
+    <h1 class="text-white">Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  
+</style>
